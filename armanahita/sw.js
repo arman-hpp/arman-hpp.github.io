21d1f73528e63b6a5bf4c898c5a2c41b7d877978
arman-hpp <arman-hpp@users.noreply.github.com>
2025-07-04 13:27:49 +0000

    deploy: 61f1e87a0c189e76dd2cb4632d3b230b7e82eb65

---
+++ armanahita/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'static-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/bg.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
